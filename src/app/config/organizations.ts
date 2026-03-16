5fdf5425edd773860fa6a72c5e0a7e827498c727
skhattab <skhattab@cs.pitt.edu>
2026-03-16 12:35:11 -0400

    Update organizations.ts

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -10,9 +10,13 @@ export const ORGANIZATIONS = {
       "🧑‍💻 Lab 3: ": "cs1501-lab3",
       "🧑‍💻 DLB Lab": "cs1501-DLB",
       "🧑‍💻 Lab 4": "cs1501-lab4",
+      "🧑‍💻 Lab 5": "cs1501-lab5",
+      "🧑‍💻 Lab 6": "cs1501-lab6",
       "🧑‍💻 Assignment 1: ": "cs1501-assignment1",
       "🧑‍💻 Assignment 2: ": "cs1501-assignment2",
       "🧑‍💻 Assignment 3: ": "cs1501-assignment3",
+      "🧑‍💻 Assignment 4: ": "cs1501-assignment4",
+      "🧑‍💻 Assignment 5: ": "cs1501-assignment5",
     }
   },
   "cs1550-2264": {
@@ -22,8 +26,12 @@ export const ORGANIZATIONS = {
       "🧑‍💻 Lab 1: ": "cs1550-lab1",
       "🧑‍💻 Lab 2: ": "cs1550-lab2",
       "🧑‍💻 Lab 3: ": "cs1550-lab3",
+      "🧑‍💻 Lab 4: ": "cs1550-lab4",
+      "🧑‍💻 Lab 5: ": "cs1550-lab5",
       "🧑‍💻 Project 1: ": "cs1550-project1",
       "🧑‍💻 Project 2: ": "cs1550-project2",
+      "🧑‍💻 Project 3: ": "cs1550-project3",
+      "🧑‍💻 Project 4: ": "cs1550-project4",
     }
   }
 } as const;
